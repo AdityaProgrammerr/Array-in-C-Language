17363c4894de8cbf2e363bfaada5b2f1cb4f634b
AdityaProgrammerr <106011806+AdityaProgrammerr@users.noreply.github.com>
2022-05-24 23:46:15 -0700

    Add files via upload

---
+++ program136.c
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+
+
+/* 4 
+ //  1 * 2 * 3 * 4   24
+ //   4 * 3 * 2 * 1   24
+*/
+
+int Factorial(int iNo)
+{
+   int iFact = 1;
+    int iCnt = 0;
+
+  for(iCnt = 1 ; iCnt <= iNo; iCnt++)
+  {
+     iFact = iFact * iCnt; 
+  }
+   return iFact;
+}
+ 
+int main()
+{
+   
+     int iValue = 0;
+     int iRet = 0;
+
+  cout<<"Enter number\n";
+  cin>>"%d",&iValue;
+
+  iRet = Factorial(iValue);
+
+  cout<<"Factorial is : "<<iRet<<endl;
+
+  return 0;
+}
